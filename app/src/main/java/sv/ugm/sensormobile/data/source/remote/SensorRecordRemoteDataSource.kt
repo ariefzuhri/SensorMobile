3d442f98034b8dd60ab06fa4c276835343c216ce
Arief Zuhri <ariefzuhri72@gmail.com>
2023-11-06 15:45:03 +0700

    [Refactor] Move a Firebase extension to util class

--- app/src/main/java/sv/ugm/sensormobile/data/source/remote/SensorRecordRemoteDataSource.kt
+++ app/src/main/java/sv/ugm/sensormobile/data/source/remote/SensorRecordRemoteDataSource.kt
@@ -1,16 +1,10 @@
 package sv.ugm.sensormobile.data.source.remote
 
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import sv.ugm.sensormobile.data.source.remote.dto.SensorRecordSnapshot
 import sv.ugm.sensormobile.data.util.RemoteResult
+import sv.ugm.sensormobile.data.util.addValueEventListenerFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -24,34 +18,9 @@ class SensorRecordRemoteDataSource @Inject constructor(
     }
     
     suspend fun getSensorRecords(): Flow<RemoteResult<List<SensorRecordSnapshot>>> {
-        return sensorRecordsRef.addValueEventListenerFlow(SensorRecordSnapshot::class.java)
+        return sensorRecordsRef.addValueEventListenerFlow(
+            SensorRecordSnapshot::class.java
+        )
     }
     
-}
-
-private suspend fun <T : Any> DatabaseReference.addValueEventListenerFlow(
-    dataType: Class<T>,
-): Flow<RemoteResult<List<T>>> {
-    return callbackFlow {
-        val listener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val values = mutableListOf<T>()
-                dataSnapshot.children.forEach { child ->
-                    val sensorRecord = child.getValue(dataType)
-                    sensorRecord?.let { values.add(it) }
-                }
-                trySend(RemoteResult.Success(values))
-            }
-            
-            override fun onCancelled(error: DatabaseError) {
-                error.toException().apply {
-                    printStackTrace()
-                    trySend(RemoteResult.Failure(this))
-                }
-                cancel()
-            }
-        }
-        val subscription = addValueEventListener(listener)
-        awaitClose { removeEventListener(subscription) }
-    }
 }
