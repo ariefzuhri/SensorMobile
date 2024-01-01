e6ed44e8555f6255f49138a0ad011a693759c462
Arief Zuhri <ariefzuhri72@gmail.com>
2024-01-01 08:46:18 +0700

    [Data - Retrofit] Add 'keep' annotation in interface to prevent R8-related crashes

--- app/src/main/java/sv/ugm/sensormobile/data/source/remote/api/ApiService.kt
+++ app/src/main/java/sv/ugm/sensormobile/data/source/remote/api/ApiService.kt
@@ -1,9 +1,11 @@
 package sv.ugm.sensormobile.data.source.remote.api
 
+import androidx.annotation.Keep
 import retrofit2.http.GET
 import sv.ugm.sensormobile.BuildConfig
 import sv.ugm.sensormobile.data.source.remote.api.response.SensorOutputsResponse
 
+@Keep
 interface ApiService {
     
     @GET(BuildConfig.sensorOutputsEndpoint)
