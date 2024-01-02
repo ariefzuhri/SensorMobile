e4517b69745323efcc5124f333cf61851b6a74bf
Arief Zuhri <ariefzuhri72@gmail.com>
2024-01-02 13:01:45 +0700

    [UI] Modify font weight in all text styles

--- app/src/main/java/sv/ugm/sensormobile/presentation/ui/designsystem/theme/Type.kt
+++ app/src/main/java/sv/ugm/sensormobile/presentation/ui/designsystem/theme/Type.kt
@@ -18,77 +18,77 @@ val Montserrat = FontFamily(
 val Typography = Typography(
     displayLarge = TextStyle(
         fontSize = 57.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     displayMedium = TextStyle(
         fontSize = 45.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     displaySmall = TextStyle(
         fontSize = 36.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     headlineLarge = TextStyle(
         fontSize = 32.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     headlineMedium = TextStyle(
         fontSize = 28.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     headlineSmall = TextStyle(
         fontSize = 24.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     titleLarge = TextStyle(
         fontSize = 22.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     titleMedium = TextStyle(
         fontSize = 16.sp,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Medium,
         fontFamily = Montserrat,
     ),
     titleSmall = TextStyle(
         fontSize = 14.sp,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Medium,
         fontFamily = Montserrat,
     ),
     bodyLarge = TextStyle(
         fontSize = 16.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     bodyMedium = TextStyle(
         fontSize = 14.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     bodySmall = TextStyle(
         fontSize = 12.sp,
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Normal,
         fontFamily = Montserrat,
     ),
     labelLarge = TextStyle(
         fontSize = 14.sp,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Medium,
         fontFamily = Montserrat,
     ),
     labelMedium = TextStyle(
         fontSize = 12.sp,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Medium,
         fontFamily = Montserrat,
     ),
     labelSmall = TextStyle(
         fontSize = 11.sp,
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Medium,
         fontFamily = Montserrat,
     ),
 )
