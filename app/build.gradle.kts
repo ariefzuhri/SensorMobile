6740601f02a05ba5b6554d38068912a419430ee0
Arief Zuhri <ariefzuhri72@gmail.com>
2024-01-03 16:03:22 +0700

    [Build] Bump app version from 1.2.1 (2) to 2.0.0 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "sv.ugm.sensormobile"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.2.1"
+        versionCode = 3
+        versionName = "2.0.0"
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
